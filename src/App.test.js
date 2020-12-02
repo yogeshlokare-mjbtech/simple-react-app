2d8cc20a214a6ae12d658af1c2d3d79488a7510e
Yogesh Lokare <yogesh.lokare@mjbtech.com>
2020-12-02 13:47:51 +0530

    Initial Commit

---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure, shallow } from "enzyme";
+import { expect } from "chai";
+import App from "./App";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+describe("App component testing", function() {
+  it("renders welcome message", function() {
+    const wrapper = shallow(<App />);
+    const message = (
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    );
+    expect(wrapper.contains(message)).to.equal(true);
+  });
+});

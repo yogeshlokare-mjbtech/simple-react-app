2525762032892e878c929db59ff4243d28165dfb
Yogesh Lokare <yogesh.lokare@mjbtech.com>
2020-12-10 15:32:52 +0530

    Added the Stripe library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./styles.css";
+import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+// Setup Stripe.js and the Elements provider
+const stripePromise = loadStripe(
+  "pk_test_51HrbZ3LQJUMKZBGdC76dOgWPTZnV5sGLLMHOoWE5KPbP3KfZGo19lxrGcaeSSvMyVFTQB0M1449GUa3kfTBdVtMZ00xWpqL8Wj"
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product">
+        <img
+          src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress"
+          alt="laptop"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 }
